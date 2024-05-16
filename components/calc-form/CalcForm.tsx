1f7faf85dbae7cbc0d4e33c1a444ae21c55c2f8b
Mykola <kovalenkokola69@gmail.com>
2024-05-16 21:15:48 +0300

    update calc form, add slider add select to choose device, period

--- components/calc-form/CalcForm.tsx
+++ components/calc-form/CalcForm.tsx
@@ -1,22 +1,28 @@
-import {FC, useState, useTransition} from 'react'
+import {FC, useEffect, useState, useTransition} from 'react';
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {Slider} from "@/components/ui/slider"; // Import the Slider component
 import FormError from "@/components/FormError";
 import FormSuccess from "@/components/FormSuccess";
 import {Button} from "@/components/ui/button";
 import {useForm} from "react-hook-form";
 import * as z from "zod";
-import {CalculateDevices} from "@/schemas";
+import {CalculateDevices, DeviceSchema} from "@/schemas";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {calculateCreate} from "@/actions/calculate-create";
+import {getDevices} from "@/actions/getDevices";
+import {Input} from "@/components/ui/input";
 
 interface ICalcFormProps {
 }
 
+const DevicesSchema = z.array(DeviceSchema);
 const CalcForm: FC<ICalcFormProps> = ({}) => {
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
-  const [isPending, startTransition] = useTransition()
+  const [devices, setDevices] = useState<z.infer<typeof DevicesSchema>>([]);
+  const [selectedDevice, setSelectedDevice] = useState<z.infer<typeof DevicesSchema>[number] | null>(null); // State for selected device
+  const [isPending, startTransition] = useTransition();
   const form = useForm<z.infer<typeof CalculateDevices>>({
     resolver: zodResolver(CalculateDevices),
     defaultValues: {
@@ -28,118 +34,147 @@ const CalcForm: FC<ICalcFormProps> = ({}) => {
       kw: '10',
       kwMonth: '10',
     },
-  })
+  });
+
+  const [currentKw, setCurrentKw] = useState<number>(Number(form.getValues('kw')) || 0);
 
   const onSubmit = (values: z.infer<typeof CalculateDevices>) => {
-    setError('')
-    setSuccess('')
+    setError('');
+    setSuccess('');
     startTransition(() => {
-      calculateCreate(values)
-        .then((data) => {
-          setError(data?.error);
-          setSuccess(data?.success);
-        })
-    })
-  }
+      calculateCreate(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  useEffect(() => {
+    getDevices()
+      .then((devices) => {
+        setDevices(devices);
+      })
+      .catch((error) => {
+        console.error("Error fetching devices:", error);
+      });
+  }, []);
+
   return (
     <div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-          <div className='space-y-4'>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
             <FormField
-              name='nameDevice'
+              name="nameDevice"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>Назва пристрою</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
+                    <Select
+                      value={field.value}
+                      onValueChange={(value) => {
+                        field.onChange(value);
+                        const device = devices.find((d) => d.nameDevice === value);
+                        setSelectedDevice(device || null);
+                      }}
                       disabled={isPending}
-                    />
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Виберіть пристрій"/>
+                      </SelectTrigger>
+                      <SelectContent>
+                        {devices.map((device) => (
+                          <SelectItem key={device.id} value={device.nameDevice}>
+                            {device.nameDevice}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
                   </FormControl>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
             <FormField
-              name='count'
+              name="count"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>Кількість</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                    />
+                    <Input {...field} disabled={isPending}/>
                   </FormControl>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
             <FormField
-              name='hoursWork'
+              name="hoursWork"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>Часи роботи</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                    />
+                    <Input {...field} disabled={isPending}/>
                   </FormControl>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
             <FormField
-              name='period'
+              name="period"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>Період</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                    />
+                    <Input {...field} disabled={isPending}/>
                   </FormControl>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
             <FormField
-              name='kw'
+              name="kw"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>кВт</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
+                    <Slider
+                      value={[Number(field.value)]}
+                      step={Number(selectedDevice?.stepKw) || 1}
+                      max={Number(selectedDevice?.stepKwMax) || 100}
+                      onValueChange={(value) => {
+                        field.onChange(String(value[0]));
+                        setCurrentKw(value[0]); // Update the current kW value
+                      }}
                       disabled={isPending}
                     />
                   </FormControl>
+                  <div className="mt-2 text-sm text-gray-500">Current kW: {currentKw}</div>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
             <FormField
-              name='kwMonth'
+              name="kwMonth"
               control={form.control}
               render={({field}) => (
                 <FormItem>
                   <FormLabel>кВт в місяць</FormLabel>
                   <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                    />
+                    <Input {...field} disabled={isPending}/>
                   </FormControl>
                   <FormMessage/>
                 </FormItem>
-              )}/>
+              )}
+            />
           </div>
           <FormError message={error}/>
           <FormSuccess message={success}/>
-          <Button type='submit' className='w-full'>
+          <Button type="submit" className="w-full">
             Розрахувати
           </Button>
         </form>
