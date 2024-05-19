5cbc23333c50c216dce9963c173486ddfe38a419
Mykola <kovalenkokola69@gmail.com>
2024-05-19 12:41:36 +0300

    wrapper link into button

--- components/empty-calculate/EmptyCalculateData.tsx
+++ components/empty-calculate/EmptyCalculateData.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 const EmptyCalculateData = () => {
   return (
     <div className='flex justify-center items-center'>
-      <Link href='/calc'>Перейти до калькуляції енергоефективності</Link>
+      <Button variant='link' aria-label='Перейти до калькуляції енергоефективності'>
+        <Link href='/calc'>Перейти до калькуляції енергоефективності</Link>
+      </Button>
     </div>
   );
 };
