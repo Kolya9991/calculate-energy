74274c44bd306118fe860a52dd639cc5a8ffdf2e
Mykola <kovalenkokola69@gmail.com>
2024-05-19 09:49:39 +0300

    centered text in calc

--- app/calc/page.tsx
+++ app/calc/page.tsx
@@ -5,7 +5,7 @@ import EnergyCalculator from "@/components/energy-calculate/EnergyCalculator";
 const CalcPage = () => {
   return (
     <div className='mt-6'>
-      <p className="mt-4 text-lg text-center max-w-2xl mb-3">
+      <p className="mt-4 text-lg text-center mb-3 flex justify-center">
         На цій сторінці можно зробити розрахунок енергоефективності пристроїв. Виберіть, будь ласка, кількість пристроїв для розрахунку.
       </p>
       <Tabs defaultValue='one' className='flex flex-col items-center w-full md:w-auto'>
