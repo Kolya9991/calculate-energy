c0564df3ecfeefd4792af7067496cc1239d351e5
Mykola <kovalenkokola69@gmail.com>
2024-05-19 12:31:02 +0300

    add components:  - Checkbox  - EmptyCalculateData update page UserInfo:  - add table for showing calculate data

--- components/auth/user-info/UserInfo.tsx
+++ components/auth/user-info/UserInfo.tsx
@@ -4,6 +4,9 @@ import {ExtendedUser} from "@/next-auth";
 import * as z from "zod";
 import {CalculateDevices} from "@/schemas";
 import {getCalculate} from "@/actions/calculate";
+import EmptyCalculateData from "@/components/empty-calculate/EmptyCalculateData";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import {Checkbox} from "@/components/ui/checkbox";
 
 interface IUserInfoProps {
   user?: ExtendedUser
@@ -16,16 +19,59 @@ const UserInfo: FC<IUserInfoProps> = ({label, user}) => {
   const [calculate, setCalculate] = useState<z.infer<typeof CalculateSchema>>([])
   useEffect(() => {
     getCalculate(user?.id)
-      .then((data)=>{
+      .then((data) => {
         setCalculate(data)
       })
-      .catch((error)=>{
+      .catch((error) => {
         console.log('Error fetching calculate data', error)
       })
   }, []);
+
+  if (!calculate) {
+    return <EmptyCalculateData/>
+  }
+
+  // id: z.string(),
+  //   nameDevice: z.string().min(1),
+  //   count: z.string(),
+  //   hoursWork: z.string(),
+  //   period: z.string(),
+  //   kw: z.string(),
+  //   kwMonth: z.string()
+
+  const headers = [
+    'Назва приладу',
+    'Кількість',
+    'Кількість годин роботи',
+    'Період',
+    'кВт',
+    'кВт в місяць',
+  ]
   return (
-    <div>
-      {JSON.stringify(calculate)}
+    <div className='overflow-x-auto w-full'>
+      <Table className='bg-white min-w-[600px] mx-auto rounded-xl h-full'>
+        <TableHeader>
+          <TableRow>
+            <TableHead><Checkbox/></TableHead>
+            {headers.map((header, index) => (
+              <TableHead key={index}>{header}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody className='text-sm text-gray-600'>
+          {calculate?.map((item, index) => (
+            <TableRow key={index} className='bg-white'>
+              <TableCell><Checkbox/></TableCell>
+              <TableCell>{item.nameDevice}</TableCell>
+              <TableCell>{item.count}</TableCell>
+              <TableCell>{item.hoursWork}</TableCell>
+              <TableCell>{item.period}</TableCell>
+              <TableCell>{item.kw}</TableCell>
+              <TableCell>{item.kwMonth}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
---
+++ components/empty-calculate/EmptyCalculateData.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const EmptyCalculateData = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <Link href='/calc'/>
+    </div>
+  );
+};
+
+export default EmptyCalculateData;
