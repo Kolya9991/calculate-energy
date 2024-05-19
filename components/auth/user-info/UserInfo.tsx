3d3d530c96cdee0b6d15aaa784fe3ca76257e837
Mykola <kovalenkokola69@gmail.com>
2024-05-19 12:38:00 +0300

    add condition add skeleton while loading data

--- components/auth/user-info/UserInfo.tsx
+++ components/auth/user-info/UserInfo.tsx
@@ -7,6 +7,7 @@ import {getCalculate} from "@/actions/calculate";
 import EmptyCalculateData from "@/components/empty-calculate/EmptyCalculateData";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {Checkbox} from "@/components/ui/checkbox";
+import {Skeleton} from "@/components/ui/skeleton";
 
 interface IUserInfoProps {
   user?: ExtendedUser
@@ -28,16 +29,8 @@ const UserInfo: FC<IUserInfoProps> = ({label, user}) => {
   }, []);
 
   if (!calculate) {
-    return <EmptyCalculateData/>
+    return <Skeleton className="min-w-[600px] h-[20px] rounded-full" />
   }
-
-  // id: z.string(),
-  //   nameDevice: z.string().min(1),
-  //   count: z.string(),
-  //   hoursWork: z.string(),
-  //   period: z.string(),
-  //   kw: z.string(),
-  //   kwMonth: z.string()
 
   const headers = [
     'Назва приладу',
@@ -48,31 +41,35 @@ const UserInfo: FC<IUserInfoProps> = ({label, user}) => {
     'кВт в місяць',
   ]
   return (
-    <div className='overflow-x-auto w-full'>
-      <Table className='bg-white min-w-[600px] mx-auto rounded-xl h-full'>
-        <TableHeader>
-          <TableRow>
-            <TableHead><Checkbox/></TableHead>
-            {headers.map((header, index) => (
-              <TableHead key={index}>{header}</TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody className='text-sm text-gray-600'>
-          {calculate?.map((item, index) => (
-            <TableRow key={index} className='bg-white'>
-              <TableCell><Checkbox/></TableCell>
-              <TableCell>{item.nameDevice}</TableCell>
-              <TableCell>{item.count}</TableCell>
-              <TableCell>{item.hoursWork}</TableCell>
-              <TableCell>{item.period}</TableCell>
-              <TableCell>{item.kw}</TableCell>
-              <TableCell>{item.kwMonth}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+    <>
+      {calculate && calculate.length ?
+        <div className='overflow-x-auto w-full'>
+          <Table className='bg-white min-w-[600px] mx-auto rounded-xl h-full'>
+            <TableHeader>
+              <TableRow>
+                <TableHead><Checkbox/></TableHead>
+                {headers.map((header, index) => (
+                  <TableHead key={index}>{header}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody className='text-sm text-gray-600'>
+              {calculate?.map((item, index) => (
+                <TableRow key={index} className='bg-white'>
+                  <TableCell><Checkbox/></TableCell>
+                  <TableCell>{item.nameDevice}</TableCell>
+                  <TableCell>{item.count}</TableCell>
+                  <TableCell>{item.hoursWork}</TableCell>
+                  <TableCell>{item.period}</TableCell>
+                  <TableCell>{item.kw}</TableCell>
+                  <TableCell>{item.kwMonth}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div> : <EmptyCalculateData/>
+      }
+    </>
   );
 };
 
--- components/empty-calculate/EmptyCalculateData.tsx
+++ components/empty-calculate/EmptyCalculateData.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const EmptyCalculateData = () => {
   return (
     <div className='flex justify-center items-center'>
-      <Link href='/calc'/>
+      <Link href='/calc'>Перейти до калькуляції енергоефективності</Link>
     </div>
   );
 };
