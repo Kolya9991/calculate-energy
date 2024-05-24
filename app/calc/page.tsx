ae29f9fad9769bfac5b458e0e4934940344c221b
Mykola <kovalenkokola69@gmail.com>
2024-05-24 07:43:05 +0300

    fix style for calcForm and EnergyCalc

--- app/calc/page.tsx
+++ app/calc/page.tsx
@@ -5,7 +5,7 @@ import EnergyCalculator from "@/components/energy-calculate/EnergyCalculator";
 const CalcPage = () => {
   return (
     <div className='mt-6'>
-      <p className="mt-4 text-lg text-center mb-3 flex justify-center">
+      <p className="mt-4 text-lg text-center mb-3 flex justify-center w-full md:max-w-[670px] md:mx-auto">
         На цій сторінці можно зробити розрахунок енергоефективності пристроїв. Виберіть, будь ласка, кількість пристроїв для розрахунку.
       </p>
       <Tabs defaultValue='one' className='flex flex-col items-center w-full md:w-auto'>
@@ -13,8 +13,8 @@ const CalcPage = () => {
           <TabsTrigger value='one'>Один пристрій</TabsTrigger>
           <TabsTrigger value='two'>Декілька пристроїв</TabsTrigger>
         </TabsList>
-        <TabsContent value='one' className='w-full'><CalcForm/></TabsContent>
-        <TabsContent value='two' className='w-full'><EnergyCalculator/></TabsContent>
+        <TabsContent value='one' className='w-full md:max-w-[670px]'><CalcForm/></TabsContent>
+        <TabsContent value='two' className='w-full md:max-w-[670px]'><EnergyCalculator/></TabsContent>
       </Tabs>
     </div>
   );
